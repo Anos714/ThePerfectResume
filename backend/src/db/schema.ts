1cd729a2ca895a55e4cd03fc755f06162d1a6d50
Rahul Sain <sainrahul374@gmail.com>
2026-08-14 20:45:10 +0530

    feat(backend): setup the neon postgres db with drizzle config file

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,32 @@
+import { sql } from "drizzle-orm";
+import {
+  boolean,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+const providerEnum = pgEnum("provider", ["google", "local"]);
+
+export const users = pgTable("users", {
+  id: uuid("id")
+    .primaryKey()
+    .default(sql`uuidv7()`),
+  googleId: varchar("google_id"),
+  provider: providerEnum("provider").default("local"),
+  avatarUrl: text("avatar_url"),
+  username: varchar("username", { length: 150 }).notNull(),
+  email: varchar("email", { length: 150 }).unique().notNull(),
+  passwordHash: text("password_hash"),
+  isVerified: boolean("is_verified").default(false),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .$onUpdateFn(() => new Date())
+    .notNull(),
+});
