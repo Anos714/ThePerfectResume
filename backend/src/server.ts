5751daa9bda2af66c35f345e2539a098880c2e1f
Rahul Sain <sainrahul374@gmail.com>
2026-08-15 19:40:23 +0530

    refactor(backend): wire up the global error & 404 handlers with userRoutes, logger, and cors in the server file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,33 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import userRoutes from "./modules/users/user.routes";
+import { logger } from "hono/logger";
+import { corsConfig } from "./config/cors";
+import { cors } from "hono/cors";
+import { errorHandler, notFoundHandler } from "./middlewares/errorHandler";
+import { env } from "./config/env";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use("*", logger());
+app.use("/api/*", cors(corsConfig));
 
-export default app
+// global error and 404 handlers
+app.onError(errorHandler);
+app.notFound(notFoundHandler);
+
+// test route (ping route)
+app.get("/ping", (c) => {
+  return c.json({
+    success: true,
+    message: "pong",
+  });
+});
+
+app.route("api/v1/users", userRoutes);
+
+export default {
+  port: Number(env.PORT) || 5000,
+  fetch: app.fetch,
+};
+
+console.log(`Server is running on port ${Number(env.PORT)}`);
