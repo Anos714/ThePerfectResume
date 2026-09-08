a39e15aab308d5af641a8e9285e21ce39a5b2750
Rahul Sain <sainrahul374@gmail.com>
2026-09-08 15:12:23 +0530

    chore(backend): chnage the origin url of cors config

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,8 +1,10 @@
+import { env } from "./env";
+
 export const corsConfig = {
-  origin: ["http://localhost:3000"],
+  origin: env.FRONTEND_URL||"http://localhost:5173",
   credentials: true,
   allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   maxAge: 86400,
   exposeHeaders: ["X-Total-Count"],
   allowHeaders: ["Content-Type", "Authorization"],
-}
+};
